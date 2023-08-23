6a7d652455b566667e8e65f8b709302fd7b16516
ecamo19 <erickcamo19@gmail.com>
2023-08-23 16:25:59 +1000

    vault backup: 2023-08-23 16:25:59

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 DroughtBox user guide",
+    pageTitle: "🪴 DroughtBox",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
